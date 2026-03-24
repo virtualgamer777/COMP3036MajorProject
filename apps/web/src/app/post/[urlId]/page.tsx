b84fcc259d8e0edc6730c163e60620f65e947e26
virtualgamer777 <lazyfredbear@gmail.com>
2026-03-24 21:44:22 +1100

    post pages are pretty much vinalised

--- apps/web/src/app/post/[urlId]/page.tsx
+++ apps/web/src/app/post/[urlId]/page.tsx
@@ -2,6 +2,8 @@ import { BlogListItem } from "@/components/Blog/ListItem";
 import { AppLayout } from "@/components/Layout/AppLayout";
 import { Post, posts } from "@repo/db/data";
 import { toUrlPath } from "@repo/utils/url";
+import { Marked, marked } from "marked";
+
 
 export default async function Page({
   params,
@@ -16,7 +18,45 @@ export default async function Page({
   }
 
   //if here
-  return BlogListItem({post});
+  return(
+    <AppLayout>
+      <article className="mx-auto max-w-3xl py-8 flex flex-col items-center">
+        <div className="p-4 flex-1 ">
+          <div className="mb-1 text-xs uppercase tracking-wide">
+            {post.date.toLocaleString("en-AU", {day: "2-digit", month: "short", year: "numeric"})} 
+            &nbsp;&nbsp;{post.category}
+          </div>
+          {post.title}
+          <div className="md:w-200 md:flex-shrink-0">
+            <img
+              src={`https://picsum.photos/seed/${post.id}/500/300`}
+              alt={post.title}
+              className="h-48 w-full object-cover md:h-full"
+            />
+          </div>
+          <div className="mt-2 text-sm text-secondary" dangerouslySetInnerHTML={{__html: await marked(post.content)}}>
+          </div>
+          <p className=" text-secondary mt-4 text-sm">
+            {"#" + post.tags.split(",").join(" #")}
+          </p>
+          <div className="mt-4 border-t border-gray-200 pt-3 text-sm">
+            <div className="flex items-center justify-between">
+              <span>
+                {(
+                  post as Post & { views?: number }
+                ).views ?? 0} views
+              </span>
+              <span>
+                {(
+                  post as Post & { likes?: number }
+                ).likes ?? 0} Likes
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+    </AppLayout>
+  );
 
   
 }
--- apps/web/src/components/Blog/ListItem.tsx
+++ apps/web/src/components/Blog/ListItem.tsx
@@ -5,47 +5,47 @@ import Link from "next/link";
 export function BlogListItem({ post }: { post: Post }) {
   return (
     <article className="flex flex-col md:flex-row">
-              {/* Image side */}
-              <Link href={`/post/${toUrlPath(post.title)}`} className="block focus:outline-none outline-none">
-                <div className="md:w-56 md:flex-shrink-0">
-                  <img
-                    src={`https://picsum.photos/seed/${post.id}/500/300`}
-                    alt={post.title}
-                    className="h-48 w-full object-cover md:h-full"
-                  />
-                </div>
-              </Link>
+      {/* Image side */}
+      <Link href={`/post/${toUrlPath(post.title)}`} className="block focus:outline-none outline-none">
+        <div className="md:w-56 md:flex-shrink-0">
+          <img
+            src={`https://picsum.photos/seed/${post.id}/500/300`}
+            alt={post.title}
+            className="h-48 w-full object-cover md:h-full"
+          />
+        </div>
+      </Link>
 
-              {/* Info side */}
-              <div className="p-4 flex-1 ">
-                <div className="mb-1 text-xs uppercase tracking-wide">
-                  {post.date.toLocaleString("en-AU", {day: "2-digit", month: "short", year: "numeric"})} 
-                  &nbsp;&nbsp;{post.category}
-                </div>
-                <a href={`/post/${toUrlPath(post.title)}`} className="block">
-                  {post.title}
-                </a>
-                <p className="mt-2 text-sm text-secondary">
-                  {post.description}
-                </p>
-                <p className=" text-secondary mt-4 text-sm">
-                  {"#" + post.tags.split(",").join(" #")}
-                </p>
-                <div className="mt-4 border-t border-gray-200 pt-3 text-sm">
-                  <div className="flex items-center justify-between">
-                    <span>
-                      {(
-                        post as Post & { views?: number }
-                      ).views ?? 0} views
-                    </span>
-                    <span>
-                      {(
-                        post as Post & { likes?: number }
-                      ).likes ?? 0} Likes
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </article>
+      {/* Info side */}
+      <div className="p-4 flex-1 ">
+        <div className="mb-1 text-xs uppercase tracking-wide">
+          {post.date.toLocaleString("en-AU", {day: "2-digit", month: "short", year: "numeric"})} 
+          &nbsp;&nbsp;{post.category}
+        </div>
+        <a href={`/post/${toUrlPath(post.title)}`} className="block">
+          {post.title}
+        </a>
+        <p className="mt-2 text-sm text-secondary">
+          {post.description}
+        </p>
+        <p className=" text-secondary mt-4 text-sm">
+          {"#" + post.tags.split(",").join(" #")}
+        </p>
+        <div className="mt-4 border-t border-gray-200 pt-3 text-sm">
+          <div className="flex items-center justify-between">
+            <span>
+              {(
+                post as Post & { views?: number }
+              ).views ?? 0} views
+            </span>
+            <span>
+              {(
+                post as Post & { likes?: number }
+              ).likes ?? 0} Likes
+            </span>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 }
