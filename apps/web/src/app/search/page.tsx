d043cf60f52ca5a17a385bfd63f3ba80cd18f51a
virtualgamer777 <lazyfredbear@gmail.com>
2026-03-25 11:19:10 +1100

    search box is working

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { AppLayout } from "@/components/Layout/AppLayout";
 import { Main } from "@/components/Main";
+import { posts } from "@repo/db/data";
 
 export default async function Page({
   searchParams,
@@ -7,10 +8,12 @@ export default async function Page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
+  //filter posts out to only those with titles or descriptions matching the search query
+  const filteredPosts = posts.filter((post) => post.title.toLowerCase().includes(q.toLowerCase()) || post.description.toLowerCase().includes(q.toLowerCase())); 
 
   return (
     <AppLayout query={q}>
-      <Main posts={[]} />
+      <Main posts={filteredPosts} />
     </AppLayout>
   );
 }
