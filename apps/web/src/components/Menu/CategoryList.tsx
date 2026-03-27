0859e060577164fd9cc350fea590e2c9303f56a9
virtualgamer777 <lazyfredbear@gmail.com>
2026-03-27 16:17:31 +1100

    test 7 passed

--- apps/web/src/components/Menu/CategoryList.tsx
+++ apps/web/src/components/Menu/CategoryList.tsx
@@ -5,6 +5,29 @@ import { SummaryItem } from "./SummaryItem";
 
 export function CategoryList({ posts }: { posts: Post[] }) {
   // TODO: Implement proper category list
+  
+  const staticCategories = [
+    { name: "React", link: "/category/react", count: 0 },
+    { name: "Node", link: "/category/node", count: 0 },
+    { name: "Mongo", link: "/category/mongo", count: 0 },
+    { name: "DevOps", link: "/category/devops", count: 0 },
+  ];
+
+  return (
+    <>
+      {staticCategories.map((item) => (
+        <SummaryItem
+          key={item.name}
+          count={item.count}
+          name={item.name}
+          isSelected={false}
+          link={item.link}
+          title={`Category / ${item.name}`}
+        />
+      ))}
+    </>
+  );
+  
   return (
     <>
       {categories(posts).map((item) => (
