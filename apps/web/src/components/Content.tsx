5a51f7ed9fb2cdd7e946885e80bca39db1460b0f
virtualgamer777 <lazyfredbear@gmail.com>
2026-05-10 15:30:36 +1000

    starting from scratch

--- apps/web/src/components/Content.tsx
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export function Content({ children }: PropsWithChildren) {
-  return <div className="flex-1 min-w-0 h-full overflow-y-auto">{children}</div>;
-}
